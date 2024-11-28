09cbaff1acbe2c79881106266de3f6ec7a0f8c92
sindri-afk <sindribjarka@gmail.com>
2024-11-28 12:36:17 +0000

    Main page initialization

---
+++ MainPage/main.py
@@ -0,0 +1,13 @@
+
+class MainPage:
+    def __init__(self):
+        self.title = "Main Page"
+
+    def show(self):
+        print(self.title)
+
+
+
+if __name__ =="__main__":
+    main_page = MainPage()
+    main_page.show()
